2d9dfffa8ab5cc90a22923ca1757ee066b4934ba
Francesca <francesca.rossi@iisviolamarchesini.edu.it>
2023-02-23 22:17:42 +0100

    Somma due numeri interi

---
+++ prova2.c
@@ -0,0 +1,10 @@
+// programma che calcola la somma di 2 numeri interi
+#include <stdio.h>
+void main()
+{
+    int add1, add2 = 3, somma; // inizializzazione della variabile
+    float var1, var2;
+    add1 = 5; // assegnazione della variabile
+    somma = var1 + var2;
+    printf("La somma è %d: ", somma);
+}
